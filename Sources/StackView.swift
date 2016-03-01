c49f8599a859aec0016d1ee42798f11140e890d8
kean <grebenyuk.alexander@gmail.com>
2016-03-01 20:43:04 +0300

    Add ViewExtension; Initial enums

--- Sources/StackView.swift
+++ Sources/StackView.swift
@@ -5,6 +5,27 @@
 import Foundation
 import UIKit
 
+public enum StackViewDistribution {
+    case Fill
+    case FillEqually
+    case EqualSpacing
+}
+
+public enum StackViewAlignment {
+    case Fill
+    case Leading
+    public static var Top: StackViewAlignment {
+        return .Leading
+    }
+    case Center
+    case Trailing
+    public static var Bottom: StackViewAlignment {
+        return .Trailing
+    }
+    case FirstBaseline
+    case LastBaseline
+}
+
 public class StackView : UIView {
 
 }
---
+++ Sources/ViewExtensions.swift
@@ -0,0 +1,15 @@
+// The MIT License (MIT)
+//
+// Copyright (c) 2016 Alexander Grebenyuk (github.com/kean).
+
+import UIKit
+
+extension UIView {
+    func sk_stackViews(views: [UIView], axis: UILayoutConstraintAxis, spacing: CGFloat, distribution: StackViewDistribution, alignment: StackViewAlignment) {
+        
+    }
+
+    private func sk_spaceViews(views: [UIView], axis: UILayoutConstraintAxis, spacing: CGFloat, strictSpacing: Bool) {
+
+    }
+}

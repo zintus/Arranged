e8aeecc39d2d21bd18d8dd235b06c5a19bd51bc9
kean <grebenyuk.alexander@gmail.com>
2016-03-01 22:38:24 +0300

    Add public methods and properties to StackView; Update Arranged demo

--- Sources/StackView.swift
+++ Sources/StackView.swift
@@ -8,7 +8,9 @@ import UIKit
 public enum StackViewDistribution {
     case Fill
     case FillEqually
+    case FillProportionally
     case EqualSpacing
+    case EqualCentering
 }
 
 public enum StackViewAlignment {
@@ -27,5 +29,67 @@ public enum StackViewAlignment {
 }
 
 public class StackView : UIView {
-
+    public var axis: UILayoutConstraintAxis = .Horizontal
+    public var distribution: StackViewDistribution = .Fill
+    public var alignment: StackViewAlignment = .Fill
+    public var spacing: CGFloat = 0.0
+    
+    // FIXME:
+    public var baselineRelativeArrangement = false
+    public var layoutMarginsRelativeArrangement = false
+    
+    private var invalidated = false
+    
+    public private(set) var arrangedSubviews: [UIView]
+    
+    public init(arrangedSubviews views: [UIView]) {
+        self.arrangedSubviews = views
+        super.init(frame: CGRectZero)
+    }
+    
+    public required init?(coder aDecoder: NSCoder) {
+        
+        self.arrangedSubviews = []
+        super.init(coder: aDecoder)
+        
+        // FIXME:
+        self.subviews.forEach{ $0.removeFromSuperview() }
+        self.subviews.forEach{ self.addArrangedSubview($0) }
+    }
+    
+    public func addArrangedSubview(view: UIView) {
+        if view.superview != view {
+            self.arrangedSubviews.append(view)
+            self.addSubview(view)
+        }
+    }
+    
+    public func removeArrangedSubview(view: UIView) {
+        // FIXME:
+    }
+    
+    public func insertArrangedSubview(view: UIView, atIndex stackIndex: Int) {
+        // FIXME:
+    }
+    
+    // MARK: Layout
+    
+    private func invalidateLayout() {
+        if !self.invalidated {
+            self.invalidated = true
+            self.setNeedsUpdateConstraints()
+        }
+    }
+    
+    public override func updateConstraints() {
+        if self.invalidated {
+            self.invalidated = false
+            self.refreshConstraints()
+        }
+        super.updateConstraints()
+    }
+    
+    private func refreshConstraints() {
+        // FIXME: Add constraints
+    }
 }

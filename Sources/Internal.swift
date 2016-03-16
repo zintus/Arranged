d0735341a568582a9980fc38e61c2be9383de106
kean <grebenyuk.alexander@gmail.com>
2016-03-16 14:45:05 +0300

    LayoutGuides are now hidden be default

--- Sources/Internal.swift
+++ Sources/Internal.swift
@@ -6,6 +6,16 @@ import UIKit
 
 
 class LayoutGuide: UIView {
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        // Make sure that layout guides don't interfere with touches
+        self.hidden = true
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
     override class func layerClass() -> AnyClass {
         return CATransformLayer.self
     }
